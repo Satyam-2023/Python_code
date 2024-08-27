17d1c0bd222cf8bd320b8f16fa4e530bee77fd89
Satyam Kumar <satyam.k@phytecembedded.in>
2024-08-27 10:18:36 +0530

    start python code

---
+++ python_code/Output_Variables.py
@@ -0,0 +1,18 @@
+x = "Python is awesome"
+print(x)
+
+X = "Python"
+Y = "is"
+Z = "awesome"
+
+print(X, Y, Z)
+
+print(X+Y+Z)
+#print(X*Y*Z)
+U = 5
+T = 10
+
+print(U+T)
+print(U*T)
+print(U/T)
+print(U%T)
---
+++ python_code/Python_Variables.py
@@ -0,0 +1,16 @@
+x = 5
+y = "hello, world!"
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+print("\n",type(x))
+print("\n",type(y))
+
+X = str(3)
+Y = int(3)
+Z = float(3)
+print("\n",X)
+print("\n",Y)
+print("\n",Z)
+	
