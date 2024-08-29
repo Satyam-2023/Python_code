d56c8202dd717b2a32033de34abb736e77990a6c
Satyam Kumar <shubh6222@gmail.com>
2024-08-29 16:10:13 +0530

    Operators and Typcasting

---
+++ python_code/Operators.py
@@ -0,0 +1,29 @@
+num1 = 10
+num2 = 2
+
+#Arithmetic operators!
+
+print("sum of num1 + num2 =", num1 + num2)
+print("sum of num1 - num2 =", num1 - num2)
+print("sum of num1 % num2 =", num1 % num2)
+print("sum of num1 * num2 =", num1 * num2)
+print("sum of num1 / num2 =", num1 / num2)
+
+#Assignment Operators!
+a = 4
+a += 2
+print("a =",a)
+
+b = 5
+b **=2
+print("b =",b)
+
+#Comparison Operator!
+
+c = 5
+d = 9
+
+print(c>d)
+print(c<d)
+print(c!=d)
+
---
+++ python_code/typcasting.py
@@ -0,0 +1,3 @@
+f = "5"
+print(f)
+print(int(f)+1)
