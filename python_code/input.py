11c4cb8ce1216ac64f36e36ed50d39917140533a
Satyam Kumar <shubh6222@gmail.com>
2024-08-30 10:40:54 +0530

    Here the code of operator, how to take input and string is available

---
+++ python_code/input.py
@@ -0,0 +1,8 @@
+name = input ("enter your name: ")
+print ("sir",name)
+print (type(name))
+
+num = input ("Enter the number:")
+print (type(num))
+print (int(num)+6)
+print (type(int(num)))
---
+++ python_code/stringdeepdive.py
@@ -0,0 +1,8 @@
+name = "satyam"
+print(name)
+print(name[0:4])
+print(name[1:5])
+print(name[2:6])
+print(name.upper())
+print(name.capitalize())
+
